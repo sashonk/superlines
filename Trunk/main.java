a43ee4bba8f2cbe48663fb22142151bf90f6d3b5
sashonk <sashonk@d7d319bb-382a-4f67-95af-1090ba45e00f>
2012-10-10 08:27:52 +0000

    add simple math ops

--- Trunk/main.java
+++ Trunk/main.java
@@ -3,7 +3,10 @@
 class main{
  public static void main(String[] argc){
   System.out.println("Hello, world!");
-
+    
+  int a = 5;
+  int b = 3;
+  System.out.println(String.format("result=%d", a+b));
  }
 
 
